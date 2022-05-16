9e39bafe6b592ab47c52012ae9f7f75dbf2ff93e
NaijingGuo <naijing0529@gmail.com>
2022-05-16 09:42:52 -0700

    fix nan, more data

--- data_train.py
+++ data_train.py
@@ -16,47 +16,133 @@ def read_no_fall(file_name):
     while index < len(trial_1)-21: 
         temp = []
         for j in range(20):
-            temp.append(trial_1.iloc[index+j, 1]) # x value
+            if trial_1.iloc[index+j, 1] != "-":
+                temp.append(trial_1.iloc[index+j, 1]) # x value
+            else:
+                temp.append(0)
         for j in range(20):
-            temp.append(trial_1.iloc[index+j, 2]) # y value
+            if trial_1.iloc[index+j, 2] != "-":
+                temp.append(trial_1.iloc[index+j, 1]) # y value
+            else:
+                temp.append(0)
         for j in range(20):
-            temp.append(trial_1.iloc[index+j, 3]) # z value
+            if trial_1.iloc[index+j, 3] != "-":
+                temp.append(trial_1.iloc[index+j, 1]) # z value
+            else:
+                temp.append(0)
         temp.append(0) # label as no fall
         data.append(temp)
         index += 10
     return data
 
 def fall(temp, start,end):
+    if end == None:
+        end =len(temp)
     for i in range(start, end):
         temp[i][-1] = 1
     return temp
 
 data = []
+
 file_name = "raw_data/IMU_trial1_Brian_no_fall.xlsx"
 temp = read_no_fall(file_name)
 data = data + temp
+
 file_name = "raw_data/IMU_trial2_Brian_no_fall.xlsx"
 temp = read_no_fall(file_name)
 data = data + temp
+
 file_name = "raw_data/IMU_trial3_Brian_no_fall.xlsx"
 temp = read_no_fall(file_name)
 data = data + temp
+
 file_name = "raw_data/IMU_trial4_Brian_no_fall.xlsx"
 temp = read_no_fall(file_name)
 data = data + temp
+
 file_name = "raw_data/IMU_trial5_Brian_fall.xlsx"
 temp = read_no_fall(file_name)
 fall(temp, 2,9)
-fall(temp, 15, 25)
+fall(temp, 15, None)
 data = data + temp
+
 file_name = "raw_data/IMU_trial6_Brian_fall.xlsx"
 temp = read_no_fall(file_name)
-fall(temp, 4,13)
+fall(temp, 4,None)
 data = data + temp
+
 file_name = "raw_data/IMU_trial7_Brian_fall.xlsx"
 temp = read_no_fall(file_name)
-fall(temp, 1,13)
+fall(temp, 1, None)
 data = data + temp
+
+file_name = "raw_data/IMU_trial8_Brian_no_fall.xlsx"
+temp = read_no_fall(file_name)
+data = data + temp
+
+file_name = "raw_data/IMU_trial9_Brian_fall.xlsx"
+temp = read_no_fall(file_name)
+fall(temp,7, 18)
+fall(temp, 23, None)
+data = data + temp
+
+file_name = "raw_data/IMU_trial10_Brian_fall.xlsx"
+temp = read_no_fall(file_name)
+fall(temp, 7, 18)
+data = data + temp
+
+file_name = "raw_data/IMU_trial1_JA_no_fall.xlsx"
+temp = read_no_fall(file_name)
+data = data + temp
+
+file_name = "raw_data/IMU_trial2_JA_no_fall.xlsx"
+temp = read_no_fall(file_name)
+data = data + temp
+
+file_name = "raw_data/IMU_trial3_JA_fall.xlsx"
+temp = read_no_fall(file_name)
+fall(temp, 15, 19)
+data = data + temp
+
+file_name = "raw_data/IMU_trial1_NG_no_fall.xlsx"
+temp = read_no_fall(file_name)
+data = data + temp
+
+file_name = "raw_data/IMU_trial2_NG_fall.xlsx"
+temp = read_no_fall(file_name)
+data = data + temp
+fall(temp, 6, 23)
+
+file_name = "raw_data/IMU_trial3_NG_fall.xlsx"
+temp = read_no_fall(file_name)
+fall(temp, 4, 13)
+fall(temp, 15, 22)
+data = data + temp
+
+file_name = "raw_data/IMU_trial1_AH_fall.xlsx"
+temp = read_no_fall(file_name)
+fall(temp, 5, 8)
+fall(temp, 12, 22)
+data = data + temp
+
+file_name = "raw_data/IMU_trial2_AH_no_fall.xlsx"
+temp = read_no_fall(file_name)
+data = data + temp
+
+file_name = "raw_data/IMU_trial3_AH_no_fall.xlsx"
+temp = read_no_fall(file_name)
+data = data + temp
+
+file_name = "raw_data/IMU_trial4_AH_fall.xlsx"
+temp = read_no_fall(file_name)
+fall(temp, 7, 17)
+data = data + temp
+
+file_name = "raw_data/IMU_trial5_AH_fall.xlsx"
+temp = read_no_fall(file_name)
+fall(temp, 9, 15)
+data = data + temp
+
 
 ## convert to df after reading all data
 df = pd.DataFrame(data)
